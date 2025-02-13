b5b3120ddd033db0dfd4f41824d8b3facf06763a
kamimartinez <kamiii.martineeez@gmail.com>
2025-02-13 13:24:51 -0600

    clase Lalo 13/2/2025 Javascript

--- labs/js/lab04_index.js
+++ labs/js/lab04_index.js
@@ -1,1 +1,14 @@
+//consola
 console.log("Aqui se pone atencion a Lalo");
+console.info("la diferencia es que aparece un icono");
+console.warn("aparece un warning en amarillo");
+console.error("aparece un error en rojo y contador de errores");
+console.assert(1 == "1");
+console.assert(1 === "1");
+
+//variables, constantes
+var comestible = "espinaca"; // variabale global, sigue existiendo, seguridad importante
+
+let flor = "rosa"; // forma moderna de declarar variables
+
+const precio = 99.99;
