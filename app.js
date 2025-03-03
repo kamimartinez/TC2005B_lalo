6625b7c8a6b6d2936446c7e55fd797b5dc0ed02f
Kamila <kamiii.martineeez@gmail.com>
2025-03-03 12:57:35 -0600

    [ADD] clase lalo lab 13 3/3/2025

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-const path = require("path");
-app.use(express.static(path.join(__dirname, "public")));
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.set("view engine", "ejs");
-app.set("views", "views");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 
 //Middleware
 app.use((request, response, next) => {
-  console.log("Middleware!");
+    console.log('Middleware!');
 
-  //Le permite a la petición avanzar hacia el siguiente middleware
-  next();
+    //Le permite a la petición avanzar hacia el siguiente middleware
+    next(); 
 });
 
-const plantasRoutes = require("./routes/plantas.routes");
+const plantasRoutes = require('./routes/plantas.routes');
 
-app.use("/plantas", plantasRoutes);
+app.use('/plantas', plantasRoutes);
 
 app.use((request, response, next) => {
-  console.log("Otro middleware!");
-
-  //Manda la respuesta
-  response.statusCode = 404;
-  response.send("No se encuentra el recurso que estás buscando");
+    console.log('Otro middleware!');
+    
+    //Manda la respuesta
+    response.statusCode = 404;
+    response.send('No se encuentra el recurso que estás buscando'); 
 });
 
 app.listen(3000);
