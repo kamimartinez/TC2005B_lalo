a36c2bdb5e11f301f38a66ec5871d59c836cd8cc
Kamila <kamiii.martineeez@gmail.com>
2025-03-03 11:08:54 -0600

    agregar archivos ejs

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,13 +22,13 @@ app.use((request, response, next) => {
 const plantasRoutes = require("./routes/plantas.routes");
 
 app.use("/plantas", plantasRoutes);
-app.use("/plants", plantasRoutes);
 
 app.use((request, response, next) => {
   console.log("Otro middleware!");
 
   //Manda la respuesta
-  response.send("¡Hola mundo!");
+  response.statusCode = 404;
+  response.send("No se encuentra el recurso que estás buscando");
 });
 
 app.listen(3000);
