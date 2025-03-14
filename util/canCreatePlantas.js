25aaa44b0a0bf0dbf03b9e72f20824216588926e
Kamila <kamiii.martineeez@gmail.com>
2025-03-14 13:24:24 -0600

    no me deja cambiar de rama sin commit

---
+++ util/canCreatePlantas.js
@@ -0,0 +1,9 @@
+module.exports = (request, response, next) => {
+    let canView = false;
+    for(let privilegio of request.session.privilegios) {
+        if(privilegio.nombre == "agregar plantas") {
+            return next();
+        }
+    }
+    return response.status(403).send("Tus acciones han sido registradas y enviadas al superAdmin");
+}
